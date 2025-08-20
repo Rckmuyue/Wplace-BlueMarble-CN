e3ab7b02bb601cc96b74a992ba9ed4cd4a70c29f
Muyue <rckmuyue@qq.com>
2025-08-20 09:56:32 +0800

    Update BlueMarble.meta.js

--- src/BlueMarble.meta.js
+++ src/BlueMarble.meta.js
@@ -1,15 +1,16 @@
 // ==UserScript==
-// @name         Blue Marble
+// @name         Blue Marble (汉化版)
 // @namespace    https://github.com/SwingTheVine/
-// @version      0.84.0
-// @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
+// @version      0.84.0-CN
+// @description  【汉化版】Blue Marble 用户脚本，用于增强 Wplace.live 体验。本脚本为 Blue Marble 的汉化分支，保留原作者 SwingTheVine 信息，遵循 MPL-2.0 协议。
 // @author       SwingTheVine
+// @translator   Muyue (汉化)
 // @license      MPL-2.0
 // @supportURL   https://discord.gg/tpeBPy46hf
-// @homepageURL  https://bluemarble.camilledaguin.fr/
-// @icon         https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/8d02ac9cbe8f6861248152f2b0d632a0b4a830ee/dist/assets/Favicon.png
-// @updateURL    https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
-// @downloadURL  https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
+// @homepageURL  https://github.com/MuyueHan/BlueMarble-CN
+// @icon         https://raw.githubusercontent.com/MuyueHan/BlueMarble-CN/main/dist/assets/Favicon.png
+// @updateURL    https://raw.githubusercontent.com/MuyueHan/BlueMarble-CN/main/dist/BlueMarbleCN.user.js
+// @downloadURL  https://raw.githubusercontent.com/MuyueHan/BlueMarble-CN/main/dist/BlueMarbleCN.user.js
 // @match        https://wplace.live/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
@@ -17,9 +18,11 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @connect      telemetry.thebluecorner.net
-// @resource     CSS-BM-File https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/8d02ac9cbe8f6861248152f2b0d632a0b4a830ee/dist/BlueMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/MuyueHan/BlueMarble-CN/main/dist/BlueMarbleCN.user.css
 // ==/UserScript==
 
-// Wplace  --> https://wplace.live
-// License --> https://www.mozilla.org/en-US/MPL/2.0/
-
+// 汉化分支说明：
+// 本脚本在原作者 SwingTheVine 的 Blue Marble 基础上进行汉化。
+// 原版仓库: https://github.com/SwingTheVine/Wplace-BlueMarble
+// 汉化仓库: https://github.com/Rckmuyue/Wplace-BlueMarble-CN
+// 许可证: Mozilla Public License Version 2.0
